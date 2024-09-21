f3debafac401d6f99919ddcfabffd63ff523bf9b
YL2209 <59274897+YL2209@users.noreply.github.com>
2024-09-21 22:51:32 +0800

    Update diy-part2-imm.sh

--- diy-part2-imm.sh
+++ diy-part2-imm.sh
@@ -30,6 +30,7 @@ rm -rf feeds/luci/themes/luci-theme-material
 rm -rf feeds/luci/themes/luci-theme-netgear
 
 git clone https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
+git clone https://github.com/jerrykuku/luci-app-argon-config.git package/luci-app-argon-config
 
 # 修改 argon 为默认主题,可根据你喜欢的修改成其他的（不选择那些会自动改变为默认主题的主题才有效果）
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' ./feeds/luci/collections/luci/Makefile
