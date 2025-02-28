023010ebe44f094732781b612a899c9d88322113
yl2209 <3021027861@qq.com>
2025-02-28 18:22:12 +0800

    first commit

---
+++ openwrt-21.02/diy-part1.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part1.sh
+# Description: OpenWrt DIY script part 1 (Before Update feeds)
+#
+# Copyright (c) 2019-2024 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+
+# Uncomment a feed source
+#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+
+# Add a feed source
+# echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+
+### 修改为R4A千兆版Breed直刷版
+AIMFILE=target/linux/ramips/dts/mt7621_xiaomi_mi-router-4a-common.dtsi
+AIDFILE=target/linux/ramips/dts/mt7621_youhua_wr1200js.dts
+[ -e "$AIMFILE" ] || AIMFILE=target/linux/ramips/dts/mt7621_xiaomi_mi-router-4a-3g-v2.dtsi
+
+MARK="// The file has been modified by@hugcabbage"
+if grep -q "$MARK" $AIMFILE; then
+    exit 0
+else
+    sed -i "1i $MARK" $AIMFILE
+fi
+
+SPECIFIC_LINE=$(sed -n '/&spi0/=' $AIMFILE)
+BASE_TEXT=$(sed '/&spi0/,/^};/d' $AIMFILE)
+
+echo "$BASE_TEXT" | sed -n "1,${SPECIFIC_LINE}p" > $AIMFILE
+sed -n '/&spi0/,/^};/p' $AIDFILE >> $AIMFILE
+echo "$BASE_TEXT" | sed -n "$SPECIFIC_LINE,\$p" >> $AIMFILE
+sed -i "${SPECIFIC_LINE}d" $AIMFILE
+
+EXFILE=target/linux/ramips/dts/mt7621_xiaomi_mi-router-4a-gigabit-v2.dts
+MT7621MK=target/linux/ramips/image/mt7621.mk
+
+if [ -e "$EXFILE" ]; then
+    sed -i 's/partition@180000/partition@50000/' $EXFILE
+    sed -i 's/0x180000 0xe70000/0x50000 0xfa0000/' $EXFILE
+    sed -i 's/partitions {/partitions: partitions {/' $AIMFILE
+    sed -i '/Device\/xiaomi_mi-router-4a-gigabit-v2/,/Mi Router 4A/ s/14784k/16000k/' $MT7621MK
+fi
+
+sed -i '/Device\/xiaomi_mi-router-4a-gigabit/,/Mi Router 4A/ s/14848k/16064k/' $MT7621MK
+sed -i '/Device\/xiaomi_mi-router-3g-v2/,/Mi Router 3G/ s/14848k/16064k/' $MT7621MK
